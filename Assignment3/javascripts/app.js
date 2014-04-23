9c3b7823c416c828858a94631c3818b9269438aa
Holly Brice <hbrice@uoregon.edu>
2014-04-22 17:23:08 -0700

    adding assignment3 to github

---
+++ Assignment3/javascripts/app.js
@@ -0,0 +1,35 @@
+
+var main = function () {
+
+    $(".tabs a span").toArray().forEach(function (element) {
+        var $element = $(element);
+
+        // create a click handler for this element
+        $element.on("click", function () {
+            
+            //Clear content in result when switching tabs
+            $(".result").empty(); 
+
+            //tabs switching stuff
+            $(".tabs a span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
+
+            if ($element.parent().is(":nth-child(1)")) {
+               renderDogTab();
+            } else if ($element.parent().is(":nth-child(2)")) {
+               renderPrimeTab();
+            } else if ($element.parent().is(":nth-child(3)")) {
+                //CANVAS DEMO
+               $(".content").append(canvas);
+            }
+
+            $(".content").append($content);
+            return false;
+
+        });
+    });
+    $(".tabs a:first-child span").trigger("click");
+};
+
+$(document).ready(main);
